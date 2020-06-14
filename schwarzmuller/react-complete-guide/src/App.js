fbb01ee6aa86d3a108aeba050a6550bc0827f51b
marcelosperalta <marcelosperalta@gmail.com>
2020-06-14 16:02:59 +0200

    updates

--- schwarzmuller/react-complete-guide/src/App.js
+++ schwarzmuller/react-complete-guide/src/App.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import './App.css';
+import Person from "./Person/Person"
 
 class App extends Component {
   render() {
@@ -7,6 +8,9 @@ class App extends Component {
       <div className="App">
         <h1>Hi, I'm a React App</h1>
         <p>This is really working!</p>
+        <Person />
+        <Person />
+        <Person />
       </div>
     );
     // return React.createElement('div', { className: 'App' }, React.createElement('h1', null, 'Does this work now?'))
