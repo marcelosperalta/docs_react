ec6418a631d543593ccb56a807481e96a2c5cd54
marcelosperalta <marcelosperalta@gmail.com>
2020-06-14 16:49:08 +0200

    updates

--- schwarzmuller/react-complete-guide/src/App.js
+++ schwarzmuller/react-complete-guide/src/App.js
@@ -3,14 +3,27 @@ import './App.css';
 import Person from "./Person/Person"
 
 class App extends Component {
+  state = {
+    persons: [
+      { name: "Max", age: 28 },
+      { name: "Manu", age: 29 },
+      { name: "Stephanie", age: 26 },
+    ]
+  }
+
+  switchNameHandler = () => {
+    console.log('Was clicked!');
+  }
+
   render() {
     return (
       <div className="App">
         <h1>Hi, I'm a React App</h1>
         <p>This is really working!</p>
-        <Person />
-        <Person />
-        <Person />
+        <button onClick={this.switchNameHandler}>Switch Name</button>
+        <Person name={this.state.persons[0].name} age={this.state.persons[0].age} />
+        <Person name={this.state.persons[1].name} age={this.state.persons[1].age} >My Hobbies: Racing</Person>
+        <Person name={this.state.persons[2].name} age={this.state.persons[2].age} />
       </div>
     );
     // return React.createElement('div', { className: 'App' }, React.createElement('h1', null, 'Does this work now?'))
--- schwarzmuller/react-complete-guide/src/Person/Person.js
+++ schwarzmuller/react-complete-guide/src/Person/Person.js
@@ -1,7 +1,13 @@
 import React from "react";
 
-const person = () => {
-    return <p>I'm a Person and I am {Math.floor(Math.random() * 30)} years old!</p>
+const person = (props) => {
+    return (
+        <div>
+            {/* this is a comment */}
+            <p>I'm a {props.name} and I am {props.age} years old!</p>
+            <p>{props.children}</p>
+        </div>
+    )
 };
 
 export default person;
