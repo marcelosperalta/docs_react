e00aff8e21de3c9618a3401888edaf3632a79fac
marcelosperalta <marcelosperalta@gmail.com>
2020-07-06 17:45:00 +0200

    React

--- cod3rCursos/fundamentos-react/src/components/Condicional.jsx
+++ cod3rCursos/fundamentos-react/src/components/Condicional.jsx
@@ -5,7 +5,7 @@ export default props => {
     return (
         <div>
             <h2>O número é {props.numero}</h2>
-            {props.numero % 2 == 0 ? <span>Par</span> : <span>Ímpar</span>}
+            {props.numero % 2 === 0 ? <span>Par</span> : <span>Ímpar</span>}
         </div>
     )
 }
--- cod3rCursos/fundamentos-react/src/components/CondicionalComIf.jsx
+++ cod3rCursos/fundamentos-react/src/components/CondicionalComIf.jsx
@@ -6,10 +6,10 @@ export default props => {
     return (
         <div>
             <h2>O número é {props.numero}</h2>
-            <If test={props.numero % 2 == 0}>
+            <If test={props.numero % 2 === 0}>
                 <span>Par</span>
             </If>
-            <If test={props.numero % 2 == 1}>
+            <If test={props.numero % 2 === 1}>
                 <span>Ímpar</span>
             </If>
         </div>
