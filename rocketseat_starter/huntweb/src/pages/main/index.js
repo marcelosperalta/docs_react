4ff701fd9e9ac8924807951ea8586950a71e1515
marcelosperalta <marcelosperalta@gmail.com>
2020-09-12 14:08:22 +0200

    react

--- rocketseat_starter/huntweb/src/pages/main/index.js
+++ rocketseat_starter/huntweb/src/pages/main/index.js
@@ -2,6 +2,8 @@ import React, { Component } from "react";
 
 import api from "../../services/api";
 
+import "./styles.css";
+
 export default class Main extends Component {
     state = {
         products: [],
@@ -19,11 +21,20 @@ export default class Main extends Component {
     };
 
     render() {
+        const { products } = this.state;
+
+
     // return <h1>Contagem de produtos: {this.state.products.length}</h1>;
         return (
             <div className="product-list">
-                {this.state.products.map(product => 
-                    (<h2 key={product._id}>{product.title}</h2>
+                {/* {this.state.products.map(product => ( */}
+                {products.map(product => (
+                // <h2 key={product._id}>{product.title}</h2>
+                <article key={product._id}>
+                    <strong>{product.title}</strong>
+                    <p>{product.description}</p>
+                    <a href="">Acessar</a>
+                </article>
                 ))}
             </div>
         )
