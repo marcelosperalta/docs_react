225bedd3903e5476ec0d19c7497422299ed68f4f
marcelosperalta <marcelosperalta@gmail.com>
2021-01-16 18:17:47 +0100

    react

--- _reactjsOrg/2_addOneMinute/like_button.js
+++ _reactjsOrg/2_addOneMinute/like_button.js
@@ -1,3 +1,29 @@
+// 'use strict';
+
+// const e = React.createElement;
+
+// class LikeButton extends React.Component {
+//   constructor(props) {
+//     super(props);
+//     this.state = { liked: false };
+//   }
+
+//   render() {
+//     if (this.state.liked) {
+//       return 'You liked this.';
+//     }
+
+//     return e(
+//       'button',
+//       { onClick: () => this.setState({ liked: true }) },
+//       'Like'
+//     );
+//   }
+// }
+
+// const domContainer = document.querySelector('#like_button_container');
+// ReactDOM.render(e(LikeButton), domContainer);
+
 'use strict';
 
 const e = React.createElement;
@@ -10,7 +36,7 @@ class LikeButton extends React.Component {
 
   render() {
     if (this.state.liked) {
-      return 'You liked this.';
+      return 'You liked comment number ' + this.props.commentID;
     }
 
     return e(
@@ -21,5 +47,13 @@ class LikeButton extends React.Component {
   }
 }
 
-const domContainer = document.querySelector('#like_button_container');
-ReactDOM.render(e(LikeButton), domContainer);
+// Find all DOM containers, and render Like buttons into them.
+document.querySelectorAll('.like_button_container')
+  .forEach(domContainer => {
+    // Read the comment ID from a data-* attribute.
+    const commentID = parseInt(domContainer.dataset.commentid, 10);
+    ReactDOM.render(
+      e(LikeButton, { commentID: commentID }),
+      domContainer
+    );
+  });
