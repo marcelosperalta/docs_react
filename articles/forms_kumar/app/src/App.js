89a17abfc150618b015a4f4b6dd063f06afed36e
Marcelo Soares Peralta <marcelosperalta@gmail.com>
2022-02-22 11:47:44 +0100

    React

--- articles/forms_kumar/app/src/App.js
+++ articles/forms_kumar/app/src/App.js
@@ -1,23 +1,38 @@
-import logo from './logo.svg';
+import { useState } from "react";
 import './App.css';
 
 function App() {
+  const [inputFields, setInputFields] = useState([
+      {name: "", age: ""}
+  ])
+
+  const handleFormChange = () => {
+
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <form>
+        {inputFields.map((input, index) => {
+          return (
+            <div key={index}>
+              <input
+                name="name"
+                placeholder="name"
+                value={input.name}
+                onChange={event => handleFormChange(index, event)}
+              />
+              <input
+                name="age"
+                placeholder="Age"
+                value={input.age}
+                onChange={event => handleFormChange(index, event)}
+              />
+            </div>
+          )
+        })}
+
+      </form>
     </div>
   );
 }
